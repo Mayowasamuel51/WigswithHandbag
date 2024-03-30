8df1c5e4afb8c9200b28203dc32219987259040d
Your Name <you@example.com>
2024-03-30 15:20:56 +0100

    added new

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -13,6 +13,12 @@ const poppins = Poppins({ subsets: ["latin"], weight: ["100", "200", "300", "400
 import Image from 'next/image'
 import landingImg from "../app/images/landing-img.jpg"
 
+export const metadata ={
+  title: "EvaTouch HomePage",
+  keywords:"",
+  content:"Evatouchbeauty: Your Trusted Global Destination for Premium Haircare and Makeup Essentials  At Evatouchbeauty, we pride ourselves on being the go-to choice for discerning individuals worldwide. Renowned for our unwavering commitment to quality and innovation, we specialize in curating the finest haircare and makeup products that elevate your beauty regimen to new heights",
+  description:"Evatouchbeauty: Your Trusted Global Destination for Premium Haircare and Makeup Essentials "
+}
 
 const Landing = () => {
   return (
